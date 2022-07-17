2877bb9328b36e86647413a9004f931bcaffec2c
Charles <charles.g.caballero@gmail.com>
2022-07-17 14:19:33 +0800

    Added request for storage page

--- modules/office/OfficeForm.tsx
+++ modules/office/OfficeForm.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 import { useForm } from "react-hook-form";
 import { FormProps } from "@helpers/interface";
 import { AddToPhotos, ModeEdit } from "@mui/icons-material";
---
+++ modules/storage-request/StorageRequest.tsx
@@ -0,0 +1,102 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { boolean, string } from "yup/lib/locale";
+import { Box, Button } from "@mui/material";
+import { Add } from "@mui/icons-material";
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number
+) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
+
+type ColumnType = string | number | boolean;
+
+interface TableHeader {
+  label: string;
+  type: ColumnType;
+}
+
+const thead: TableHeader[] = [
+  {
+    label: "Box Code",
+    type: "string",
+  },
+  {
+    label: "Document Title and Description",
+    type: "string",
+  },
+  {
+    label: "Document Date",
+    type: "string",
+  },
+  {
+    label: "Disposal Date",
+    type: "string",
+  },
+];
+
+export default function StorageRequest() {
+  return (
+    <Box>
+      <Box display={"flex"} flexDirection="row-reverse">
+        <Button variant="contained" endIcon={<Add />} sx={{ m: 1 }}>
+          Add Box
+        </Button>
+      </Box>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {thead.map((header, index) => {
+                return (
+                  <TableCell
+                    key={index}
+                    sx={{ fontWeight: "bold", textAlign: "center" }}
+                  >
+                    {header.label}
+                  </TableCell>
+                );
+              })}
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+}
